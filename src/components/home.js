2643fefcffa5550dc3e9b0b795a9f18e45ba72b2
roms <rusirusamaraweera749@gmail.com>
2023-04-22 11:10:40 +0530

    update remove feature

--- src/components/home.js
+++ src/components/home.js
@@ -43,48 +43,59 @@
 
 // export default Home;
 
-import {useState} from 'react';
+import { useState } from "react";
 
 const Home = () => {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
-const [inputText, setInputText] = useState('');
-const [items, setItems] = useState([]);
 
-const handleChange = (event) => {
+  const handleChange = (event) => {
     const newValue = event.target.value;
     setInputText(newValue);
-}
+  };
 
-const addItem = () => {
-        setItems((prevItems) => {
-          return [...prevItems, inputText];
-        });
-        setInputText("");
-      };
+  const addItem = () => {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  };
 
-    return ( 
-        <div className="container">
-              <div className="heading">
-                 <h1>To-Do list</h1>
-               </div>
-               <input   
-                    onChange={handleChange}
-                    type="text"
-                    placeholder="Add items"
-                    value = {inputText}
-               />
-               <button onClick={addItem} type="submit">
-                 Add
-               </button>
-               <div>
-                <ul>
-                    {items.map((todoItems) => (
-                        <li>&hearts; {todoItems}</li>
-                    ))}
-                </ul>
-               </div>
-        </div>
-     );
-}
- 
+  const deleteItem = (index) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item, i) => i !== index);
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do list</h1>
+      </div>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="Add items"
+        value={inputText}
+      />
+      <button className="btn" onClick={addItem} type="submit">
+        Add
+      </button>
+      <div>
+        <ul>
+          {items.map((todoItems,index) => (
+            <li key = {index} style={{textDecoration: todoItems.textDecoration}}>
+              &hearts; {todoItems}{" "}
+              <button onClick={() => deleteItem(index)} className="btnDelete">
+                ×
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
